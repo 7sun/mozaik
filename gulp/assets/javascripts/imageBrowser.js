a4e3e1fb53b40b303211f098f87ca7d588d9bcf6
7sun <pakrouse@gmail.com>
2015-12-20 18:59:37 -0800

    adds gallery view

--- gulp/assets/javascripts/imageBrowser.js
+++ gulp/assets/javascripts/imageBrowser.js
@@ -1,17 +1,29 @@
 $(function(){
-  console.log('loaded image browser');
-  $(document).on('click', '.gallery', function(){
-    // $('.image-box').removeClass('selected');
-    $(this).addClass('selected');
-    $(this).removeClass('gallery');
-    // $('body').css('overflow', 'hidden');
-    console.log(this);
+  // console.log('loaded image browser');
+  // $(document).on('click', '.gallery', function(){
+  //   $('.image-box').removeClass('selected');
+  //   $(this).addClass('selected');
+  //   $(this).removeClass('gallery');
+  //   // $('body').css('overflow', 'hidden');
+  //   console.log(this);
+  // })
+  // $(document).on('click', '.selected', function(){
+  //   $(this).addClass('gallery');
+  //   $('.image-box').removeClass('selected');
+  //   // $('body').css('overflow', 'auto');
+  //   console.log('click selected');
+  //   console.log(this);
+  // })
+  $(document).on('click', '.image2', function(){
+    $(this).toggleClass('expand');
+    $('.image3').toggleClass('contract');
   })
-  $(document).on('click', '.selected', function(){
-    $(this).addClass('gallery');
-    $(this).removeClass('selected');
-    // $('body').css('overflow', 'auto');
-    console.log('click selected');
-    console.log(this);
-  })
+
+  // $(document).on('click', '#btn', function(){
+  //   console.log("clicked button");
+  //   $('html, body').animate({
+  //     scrollTop: $("#img").offset().top
+  //   }, 1000);
+  // })
+
 })
